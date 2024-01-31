caec66d6634456900849c86b207d8c62d05dbe98
Chatura Kuruppu <ckuruppu@spinquestgpvm01.fnal.gov>
2024-01-30 18:17:01 -0600

    Updated Proxy Settings according to the latest instructions provided in https://cdcvs.fnal.gov/redmine/projects/fife/wiki/Authentication#Authentication-with-kcron-for-SL7

--- renew_proxy.sh
+++ renew_proxy.sh
@@ -7,22 +7,19 @@ echo -e "\ncleaning up existing proxy ...\n"
 voms-proxy-destroy
 
 echo -e "\ncigetcert ...\n" 
-cigetcert -vs fifebatch.fnal.gov -o ${X509_USER_PROXY} && \
+kx509 -o ${X509_USER_PROXY} && \
 echo "got certigicate" || \
-(exitcode=$?; echo -n "Error while getting certigicate\ncigetcert exited with exit code: $exitcode"; exit $exitcode)
-
-echo -e "\nDisplay info about MyProxy credentials for user ${USER}...\n" 
-myproxy-info -s myproxy.fnal.gov -vd
+(exitcode=$?; echo -e "Error while getting certigicate\ncigetcert exited with exit code: $exitcode"; exit $exitcode)
 
 echo -e "\nCreate the proxy for user ${USER} with role ${ROLE} ...\n" 
-voms-proxy-init -noregen -rfc -voms fermilab:/fermilab/spinquest/Role=${ROLE} -valid 120:00 && \
+voms-proxy-init -noregen -rfc -voms fermilab:/fermilab/nova/Role=${ROLE} -valid 120:00 && \
 echo "proxy created" || \
-(exitcode=$?; echo -n "Error while creating proxy\nvoms-proxy-init exited with exit code: $exitcode"; exit $exitcode)
+(exitcode=$?; echo -e "Error while creating proxy\nvoms-proxy-init exited with exit code: $exitcode"; exit $exitcode)
 
 echo -e "\nDisplay proxyinfo ...\n" 
 voms-proxy-info -all && \
 echo "the proxy is OK" || \
-(exitcode=$?; echo -n "Error while checking proxy\nvoms-proxy-info exited with exit code: $exitcode"; exit $exitcode)
+(exitcode=$?; echo -e "Error while checking proxy\nvoms-proxy-info exited with exit code: $exitcode"; exit $exitcode)
 
 echo "Updating the proxy" 
 mv ${X509_USER_PROXY} /var/tmp/${USER}.${ROLE}.proxy
